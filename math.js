95b5ed5652bac75ccb904d935ad45b7a8c1ab7c1
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-07-23 08:19:03 +0600

    area of circle calculator

--- math.js
+++ math.js
@@ -1,5 +1,8 @@
 // Math 1
 var eggPrice = 5;
-var  numberOfEggs = 10;
-var totalCostOfEggs = eggPrice * numberOfEggs;
-console.log("Total Eggs Price Is: ",totalCostOfEggs,"TK");
+var totalEggs =10;
+var toTatalEggsPrice = eggPrice * totalEggs;
+
+console.log("Total Eggs Price Is: ",toTatalEggsPrice,"Taka");
+
+// area of a circle calculator
