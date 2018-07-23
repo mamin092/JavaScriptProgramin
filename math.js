fc98f5791156a7f8f2321109a40f91db1e5c03cb
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-07-23 08:48:39 +0600

    area and circle

--- math.js
+++ math.js
@@ -5,4 +5,9 @@ var toTatalEggsPrice = eggPrice * totalEggs;
 
 console.log("Total Eggs Price Is: ",toTatalEggsPrice,"Taka");
 
-// area of a circle calculator
+// area of a circle calculator = pi * r squared
+
+var radius = 7;
+const pi = 3.14159;
+var areaOfCircle = pi *(radius * radius);
+console.log("Area of circle is ", areaOfCircle,"sq inch");
