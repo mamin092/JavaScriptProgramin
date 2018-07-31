0451235b5f0768a00373faf77e47dd7848b53e79
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-07-31 06:46:28 +0600

    comparison programm

---
+++ comparison.js
@@ -0,0 +1,30 @@
+var n = 71;
+if (n<10){
+    console.log(n);
+    console.log("this number ",n,"is less than 10:");
+}
+
+else if (n<20) {
+    console.log("this number ", n, "is less than 10: ");
+}
+
+else{
+    console.log(n);
+    console.log("this number ",n,"is gater than 10:");
+}
+
+
+//2
+var n = 21;
+var m = 22;
+
+if (n>m) {
+    console.log("n is gater than m",n,m);
+}
+else if(n>=m){
+console.log("n is queal m : ", n,m)
+}
+else{
+    console.log("n is less m", n,m);
+}
+
