6e2b1097f3a4c7e05b6e03297f40cac10f052fba
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-07-31 07:56:13 +0600

    comparison2

---
+++ comparison2.js
@@ -0,0 +1,66 @@
+var personOne = 'Raju';
+var personTow = 'Rony';
+var theyAreBorther = true;
+/*
+if ('Raju'==personOne) {
+    if ('Rony'==personTow) {
+        if (theyAreBorther) {
+            console.log("Raju & Rony They are brother");
+        }
+        else{
+            console.log("they are not brother");
+        }
+    }
+}
+if ("Raju"==personOne && "Rony"== personTow && theyAreBorther) {
+    console.log("The are brother ");
+}
+else{
+    console.log("the are not brother");
+}
+
+*/
+// priem number 2 3 5 7
+
+
+
+/* 
+var n = 20;
+
+if(n<10){
+    if (2==n || 3==n || 5==n ||7==n) {
+        
+        console.log("This number ", n ,"is smaller than 10 and it's a prime number");
+    }
+    else{
+        console.log("This number ", n,"is smaller than 10 and it's not prime number");
+    }
+}
+else{
+    console.log("This number ",n ,"is greater than 10");
+}
+
+
+*/
+
+
+var n = 8;
+
+if(n<10 &&(2==n ||3==n || 5==n|| 7==n)){
+console.log("This number",n ,"is smaller than 10 it's a prime number");
+
+}else if(n<10 ){
+
+    console.log("This number",n, "is smaller than 10 it's  not prime number");
+}
+else{
+    console.log("This number",n,"Greater than 10");
+}
+
+
+
+
+
+
+
+
