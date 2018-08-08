bf1699d3b4521ed0bf8542978a21cb626b80578b
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-08-08 20:06:58 +0600

    nagetive or positive

--- math2.js
+++ math2.js
@@ -1,9 +1,11 @@
-var x = 4;
 
-x *= 2;
-console.log(x);
+var n = 7;
 
-var x;
-var y = 4;
-x=y--;
-console.log("x = ",x, "y = ", y);
+if(n<10 && (2==n || 3==n || 5==n || 7==n)){
+console.log("This number",n,"is smaller than 10. it's a prime number");
+}else if(n<10){
+console.log("This number ", n,"is smaller than 10. it's not a prime number");
+}
+else{
+    console.log("This number",n,"is greater thand 10");
+}
---
+++ nagetive-or-positive.js
@@ -0,0 +1,10 @@
+var n = 13;
+
+if (n>0) {
+    console.log("this number ",n,"is positive");
+}
+else{
+    console.log("this number",n,"is negetive");
+}
+
+
