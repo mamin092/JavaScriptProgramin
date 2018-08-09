7b340f52a09b2fc2aa751fdd06ec3c399a275121
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-08-09 18:20:20 +0600

    ternary opaetor

---
+++ ternary-oparetor.js
@@ -0,0 +1,16 @@
+// var number =12;
+
+// var result ;
+// var remainder = number % 2;
+
+
+// result = (0==remainder)? "even" :"Odd";
+// console.log("this number is",result);
+
+
+var n =10;
+var result;
+
+result =(n<0)?"nagetive":"positive";
+
+console.log("this number is", result);
