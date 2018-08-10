33663ddb82b182d5bd72f3da5841c24cbfc26095
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-08-10 08:21:36 +0600

    multipale loop

--- forloop.js
+++ forloop.js
@@ -1,7 +1,14 @@
 
 
-var i = 10;
+// var i = 10;
 
-for(var i=1; i<=10; i+=2){
-console.log(i);
+// for(var i=1; i<=10; i+=2){
+// console.log(i);
+// }
+
+var i,j;
+
+for(i=0,j=10;i<=10;i++,j--){
+
+    console.log(i,j);
 }
