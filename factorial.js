01115d421785a70c3c1cfd3105cc8915fa62f450
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-08-10 10:43:04 +0600

    factorial of 10

---
+++ factorial.js
@@ -0,0 +1,20 @@
+// var n = 5;
+
+// var factorial = 1;
+
+// for (var i=n;i>1;i--) { 
+//     factorial *=i;
+    
+// }
+// console.log(factorial);
+
+
+
+//var i= 10
+
+var  factorial = 1;
+
+for(i=1;i<=10;i++){
+factorial *=i;
+console.log("Factorial of",i,'is',factorial);
+}
